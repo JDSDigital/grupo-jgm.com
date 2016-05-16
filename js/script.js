c4bc33110790822766eabd10c93aca8a18be187b
Daniel Sosa <jdsosa@gmail.com>
2016-05-16 09:28:21 -0400

    Clean garbage code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-jQuery(document).ready(function(){
-	var accordionsMenu = $('.cd-accordion-menu');
-
-	if( accordionsMenu.length > 0 ) {
-
-		accordionsMenu.each(function(){
-			var accordion = $(this);
-			//detect change in the input[type="checkbox"] value
-			accordion.on('change', 'input[type="checkbox"]', function(){
-				var checkbox = $(this);
-				console.log(checkbox.prop('checked'));
-				( checkbox.prop('checked') ) ? checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300) : checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
-			});
-		});
-	}
-});
-
 $(document).ready(function(){
 
 	$('ul.tabs li').click(function(){
