1135bc35da5b76414b6abd2343842493f0bac1cc
Daniel Sosa <jdsosa@gmail.com>
2016-05-27 14:33:09 -0430

    Add 5 images to the front gallery and an accordion menu to Nuestros Productos

--- js/script.js
+++ js/script.js
@@ -14,46 +14,18 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 
-	$('ul.tabs-2 li').click(function(){
+	$('.listaServicios li').click(function(){
 		var tab_id = $(this).attr('data-tab');
 
-		$('ul.tabs-2 li').removeClass('current-2');
-
+		$('.listaServicios li').removeClass('current-2');
+		$('.tab-content-2').removeClass('current-2');
 
 		$(this).addClass('current-2');
+		$("#"+tab_id).addClass('current-2');
 
 	})
 
 })
-
-$(document).ready(function(){
-
-	$('ul.version-menu li').click(function(){
-		var tab_id = $(this).attr('data-tab');
-
-		$('ul.version-menu li').removeClass('actual');
-		$('.contenido').removeClass('actual');
-
-		$(this).addClass('actual');
-		$("#"+tab_id).addClass('actual');
-	})
-
-})
-
-$(document).ready(function(){
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-});
 
 /* JSSOR Start */
 
