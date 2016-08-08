dfbc55199638a87a3b9e2ad9eaf7072b96d857ea
Daniel Sosa <jdsosa@gmail.com>
2016-08-08 11:13:57 -0400

    Fix Facebook Link

--- enviar.php
+++ enviar.php
@@ -20,47 +20,102 @@
 		</header>
 	</div>
 
-	<div class="row">
-		<nav id="top-menu">
-			<ul>
-				<li><a href="./index.html">Inicio</a></li>
-				<li><a href="./quienes_somos.html">Quiénes Somos</a></li>
-				<li><a href="./nuestros_productos.html">Nuestros Productos</a></li>
-				<li><a href="./contactanos.html">Contáctanos</a></li>
-			</ul>
-		</nav>
+	<div class="row navbar-menu">
+			<div class="col-xs-6 col-xs-offset-3 col-centered">
+				<nav class="top-menu">
+					<ul>
+						<li><a href="./index.html">Inicio</a></li>
+						<li><a href="./quienes_somos.html">Quiénes Somos</a></li>
+						<li><a href="./nuestros_productos.html">Nuestros Productos</a></li>
+						<li><a href="./contactanos.html">Contáctanos</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class="col-xs-2">
+				<nav class="top-menu-logo">
+					<ul>
+						<li style="padding:0;"><a href="https://www.facebook.com/people/Distribuidora-Jgm/100008156957083" target="_blank"><img class="socialLogo" src="./img/FB-f-Logo__blue_29.png"></a></li>
+						<li style="padding:0;"><a href="https://twitter.com/grupojgm" target="_blank"><img class="socialLogo" src="./img/TwitterLogo.png"></a></li>
+					</ul>
+				</nav>
+			</div>
 	</div>
 
 	<main class="center-block">
+
 		<div class="row">
-			<div class="col-xs-6 col-xs-offset-2 col-centered">
-				<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script><div style="overflow:hidden;height:500px;width:600px;"><div id="gmap_canvas" style="height:500px;width:600px;"><style>#gmap_canvas img{max-width:none!important;background:none!important}</style><a class="google-map-code" href="http://wordpress-themes.org" id="get-map-data">wordpress-themes.org</a></div></div><script type="text/javascript"> function init_map(){var myOptions = {zoom:16,center:new google.maps.LatLng(10.488567395724235,-66.87122330157467),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(10.488567395724235, -66.87122330157467)});infowindow = new google.maps.InfoWindow({content:"<b>Grupo JGM</b><br/>Av. Orinoco<br/> Caracas" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+			<div class="col-xs-8 col-xs-offset-2 contact">
+			<div class="row">
+
+
+					<div class="col-xs-7">
+
+					<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script><div class="img-responsive" style="overflow:hidden;height:400px;width:500px;margin-left:50px;"><div class="img-responsive" id="gmap_canvas" style="height:400px;width:500px;"><style>#gmap_canvas img{max-width:none!important;background:none!important}</style><a class="google-map-code" href="http://wordpress-themes.org" id="get-map-data">wordpress-themes.org</a></div></div><script type="text/javascript"> function init_map(){var myOptions = {zoom:16,center:new google.maps.LatLng(10.488567395724235,-66.87122330157467),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(10.488567395724235, -66.87122330157467)});infowindow = new google.maps.InfoWindow({content:"<b>Grupo JGM</b><br/>Av. Orinoco, Qta. Lourdes<br/>Bello Monte<br/>Caracas" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+
+					</div>
+
+					<div class="col-xs-2 col-xs-offset-1 contactData">
+
+						<img class="img-responsive logo" src="./img/logo.gif"/><br />
+
+						<div class="contactDataText">
+							<p>Rif: J-00355387-5</p>
+							<p class="contactDataTextHover"><b>jgmgrupo@gmail.com</b></p>
+							<br/>
+							<p><b>Telefonos:</b></p>
+							<p class="contactDataTextHover">+58 212 953 81 82</p>
+							<p class="contactDataTextHover">+58 212 952 98 59</p>
+							<p class="contactDataTextHover">+58 212 951 24 03</p>
+							<p class="contactDataTextHover">+58 212 951 25 91</p>
+							<p><b>Fax:</b><p>
+							<p class="contactDataTextHover">+58 212 953 11 26</p>
+						</div>
+
+					</div>
+
+
 			</div>
-			<div class="col-xs-2">
 
-				<?php
-				$nombre = $_POST['nombre'];
-				$apellido= $_POST['apellido'];
-				$mail = $_POST['email'];
+			<div class="row">
 
-				$header = 'From: ' . $mail . " \r\n";
-				$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-				$header .= "Mime-Version: 1.0 \r\n";
-				$header .= "Content-Type: text/plain";
+				<div class="col-xs-6 col-xs-offset-3">
 
-				$mensaje = "Este mensaje fue enviado por " . $nombre ." ". $apellido.  " \r\n";
-				$mensaje .= "Su e-mail es: " . $mail . " \r\n";
-				$mensaje .= "Mensaje: " . $_POST['comentario'] . " \r\n";
-				$mensaje .= "Enviado el " . date('d/m/Y', time());
 
-				$para = 'jdsosa@gmail.com'; // aqui se cambia el correo para que el cliente reciba el correo
-				$asunto = 'Asunto del mail recibido';
 
-				mail($para, $asunto, utf8_decode($mensaje), $header);
+				<form role="form" method="POST" action="enviar.php">
 
-				echo 'Mensaje enviado correctamente';
-				?>
+					<div class="form-group">
+						
+						<?php
+						$nombre = $_POST['nombre'];
+						$apellido= $_POST['apellido'];
+						$mail = $_POST['email'];
 
+						$header = 'From: ' . $mail . " \r\n";
+						$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+						$header .= "Mime-Version: 1.0 \r\n";
+						$header .= "Content-Type: text/plain";
+
+						$mensaje = "Este mensaje fue enviado por " . $nombre ." ". $apellido.  " \r\n";
+						$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+						$mensaje .= "Mensaje: " . $_POST['comentario'] . " \r\n";
+						$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+						$para = 'jdsosa@gmail.com'; // aqui se cambia el correo para que el cliente reciba el correo
+						$asunto = 'Asunto del mail recibido';
+
+						mail($para, $asunto, utf8_decode($mensaje), $header);
+
+						echo 'Mensaje enviado correctamente';
+						?>				
+
+					</div>
+
+				</form>
+
+				</div>
+
+			</div>
 			</div>
 		</div>
 	</main>
